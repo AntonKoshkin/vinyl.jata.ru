56f76cac0f77e6e283d3a5a76b21526445dccc14
scink <scink666@gmail.com>
2017-01-30 17:38:19 +0300

    добавляет фичи, шаблон секций, тайтлы, первую секцию и кусок второй

--- gulp/gulp img.js
+++ gulp/gulp img.js
@@ -3,6 +3,7 @@
 const config	= require('./config');
 // const debug		= require('gulp-debug');
 const gulp		= require('gulp');
+const gulpIf	= require('gulp-if');
 const imageMin	= require('gulp-imagemin');
 const newer		= require('gulp-newer');
 const plumber	= require('gulp-plumber');
@@ -10,27 +11,31 @@ const pngQuant	= require('imagemin-pngquant');
 const server	= require('browser-sync');
 
 module.exports = function() {
+	const production	= process.env.NODE_ENV === 'production';
 	return function() {
 		return gulp
 			.src(config.pathTo.src.img)
 			.pipe(plumber())
 			.pipe(newer(config.pathTo.build.img))
-			.pipe(imageMin({
-				intarlaced       : true,
-				optimizationLevel: 5,
-				progressive      : true,
-				arithmetic       : true,
-				svgoPlugins      : [{
-					removeViewBox: true,
-				}],
-				use: [
-					pngQuant({
-						quality: '80-90',
-						speed  : 5,
-						verbose: true,
-					})
-				],
-			}))
+			.pipe(gulpIf(
+				production,
+				imageMin({
+					intarlaced       : true,
+					optimizationLevel: 5,
+					progressive      : true,
+					arithmetic       : true,
+					svgoPlugins      : [{
+						removeViewBox: true,
+					}],
+					use: [
+						pngQuant({
+							quality: '80-90',
+							speed  : 6,
+							verbose: true,
+						})
+					],
+				})
+			))
 			// .pipe(debug())
 			.on('data', function(file) {
 				const a = file.path.split(/\/|\\/);
